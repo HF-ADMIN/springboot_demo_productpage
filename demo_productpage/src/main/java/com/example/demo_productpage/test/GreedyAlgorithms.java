27b18a8e5cad5452f3c42ba07f98b806d515ceda
soondo_kwon <kwonsd21@gmail.com>
2021-04-05 17:53:45 +0900

    1

--- demo_productpage/src/main/java/com/example/demo_productpage/test/GreedyAlgorithms.java
+++ demo_productpage/src/main/java/com/example/demo_productpage/test/GreedyAlgorithms.java
@@ -62,37 +62,105 @@ public class GreedyAlgorithms {
     /**
      * @Description
      */
+    
     // Complete the marcsCakewalk function below.
-    static long marcsCakewalk(int[] calorie) {
+    // static long marcsCakewalk(int[] calorie) {
 
-        return 1;
-    }
+    //     return 1;
+    // }
 
-    private static final Scanner scanner = new Scanner(System.in);
+    // private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+    // public static void main(String[] args) throws IOException {
+    //     BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+    //     int n = scanner.nextInt();
+    //     scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int[] calorie = new int[n];
+    //     int[] calorie = new int[n];
 
-        String[] calorieItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+    //     String[] calorieItems = scanner.nextLine().split(" ");
+    //     scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        for (int i = 0; i < n; i++) {
-            int calorieItem = Integer.parseInt(calorieItems[i]);
-            calorie[i] = calorieItem;
-        }
+    //     for (int i = 0; i < n; i++) {
+    //         int calorieItem = Integer.parseInt(calorieItems[i]);
+    //         calorie[i] = calorieItem;
+    //     }
 
-        long result = marcsCakewalk(calorie);
+    //     long result = marcsCakewalk(calorie);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
+    //     bufferedWriter.write(String.valueOf(result));
+    //     bufferedWriter.newLine();
 
-        bufferedWriter.close();
+    //     bufferedWriter.close();
 
-        scanner.close();
-    }
+    //     scanner.close();
+    // }
+
+
+    /**
+     * @Description 55533333 구하기 문제
+     */
+
+    // Complete the decentNumber function below.
+    // static void decentNumber(int n) {
+    //     StringBuffer outputString = new StringBuffer();
+
+    //     int c = (n*2%3);
+    //     if(n < c*5) outputString.append("-1");
+    //     else {
+    //         for(int i = 0; i < n- (5*c); i++) outputString.append("5");
+    //         for(int i = 0; i < 5*c; i++) outputString.append("3");
+    //     }
+    //     System.out.println(outputString);
+    // }
+
+
+    /**
+     * @Description 주어진 배열에서 4의 가중치를 두고 필요한 컨테이너의 갯수를 구하라
+     */
+
+    // Complete the toys function below.
+    // static int toys(int[] w) {
+    //     int conNum = 1;
+        
+
+    //     Arrays.sort(w);
+
+    //     int temp = w[0];
+
+    //     for(int i = 0; i < w.length; i++) {
+    //         System.out.println("index i : " + i + ", conNum : " + conNum + ", add Con : " + w[i]);
+    //         if(w[i] > temp+4) {
+    //             conNum++;
+    //             temp = w[i];
+    //         }
+    //     }
+    //     return conNum;
+    // }
+
+    // private static final Scanner scanner = new Scanner(System.in);
+
+    // public static void main(String[] args) throws IOException {
+
+
+    //     int n = scanner.nextInt();
+    //     scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    //     int[] w = new int[n];
+
+    //     String[] wItems = scanner.nextLine().split(" ");
+    //     scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    //     for (int i = 0; i < n; i++) {
+    //         int wItem = Integer.parseInt(wItems[i]);
+    //         w[i] = wItem;
+    //     }
+
+    //     int result = toys(w);
+
+    //     scanner.close();
+
+    //     System.out.println(result);
+    // }
 }
